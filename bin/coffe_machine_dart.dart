839a193364b4fd8acdbcf9be3ed387edb384ea19
abdalla3yash <abdo3yash@gmail.com>
2022-12-20 04:48:38 -0800

    first commit

---
+++ bin/coffe_machine_dart.dart
@@ -0,0 +1,5 @@
+import 'package:coffe_machine_dart/coffe_machine_dart.dart' as coffe_machine_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${coffe_machine_dart.calculate()}!');
+}
---
+++ test/coffe_machine_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:coffe_machine_dart/coffe_machine_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
